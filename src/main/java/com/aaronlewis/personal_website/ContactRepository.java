e2f3d92a2599f8714c4b2dbb41f7da1e9e52e23c
Aaron Lewis <aaron11125@gmail.com>
2021-06-15 01:19:20 -0700

    updated cross origin uri

--- src/main/java/com/aaronlewis/personal_website/ContactRepository.java
+++ src/main/java/com/aaronlewis/personal_website/ContactRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin(origins = "https://www.aaronlewis.dev/*", maxAge = 3600)
+@CrossOrigin(origins = "https://www.aaronlewis.dev", maxAge = 3600)
 @RepositoryRestResource(collectionResourceRel = "contact", path = "contact")
 public interface ContactRepository extends MongoRepository<Contact, String> {
 	
