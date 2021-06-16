f0b22554d14e038927e7d7c29325f92fc2b819d0
Aaron Lewis <aaron11125@gmail.com>
2021-06-15 18:04:40 -0700

    Updated crossorigin to include dev and prod uri

--- src/main/java/com/aaronlewis/personal_website/PersonalWebsiteApplication.java
+++ src/main/java/com/aaronlewis/personal_website/PersonalWebsiteApplication.java
@@ -2,9 +2,10 @@ package com.aaronlewis.personal_website;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class PersonalWebsiteApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(PersonalWebsiteApplication.class, args);
 	}
